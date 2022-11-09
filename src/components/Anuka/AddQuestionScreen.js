6a7af92c0640eb5f75c0cf77df7edd6635f46fcd
itanuka <it20149994@my.sliit.lk>
2022-11-10 01:00:34 +0530

    Add Question Completed

--- src/components/Anuka/AddQuestionScreen.js
+++ src/components/Anuka/AddQuestionScreen.js
@@ -1,52 +1,73 @@
-import { Image, ImageBackground, Modal, StatusBar, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React, { Component } from 'react'
+import { Image, ImageBackground, Modal, StatusBar, StyleSheet, Text, TouchableOpacity, View, Picker } from 'react-native'
+import React, { useState, Component } from 'react'
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import { Button, Card, List, TextInput } from 'react-native-paper';
-import database from '@react-native-firebase/database';
-import { UserID } from '../../Constant';
+import { useNavigation } from "@react-navigation/native";
+// import database from '@react-native-firebase/database';
+// import { UserID } from '../../Constant';
 
-export default class AddQuestionScreen extends Component {
+import {
+    collection,
+    getDocs,
+    addDoc,
+    setDoc,
+    doc,
+    getDoc,
+} from "firebase/firestore";
+import { db } from "../../Firebase/Firebase-config";
 
-    constructor(props) {
-        super(props);
 
-        this.state = {
-            title: '',
-            category: '',
-            description: '',
-            showModel: false
+export default function AddQuestionScreen() {
 
+    const navigation = useNavigation();
+    const questionsCollection = collection(db, 'questions')
+
+    const [title, setTitle] = useState("");
+    const [category, setCategory] = useState("");
+    const [description, setDescription] = useState("");
+    const [showModel, setShowModel] = useState("");
+
+    const handleSubmit = async () => {
+        try {
+            const response = await addDoc(questionsCollection, { title, category, description });
+            alert("Question Published Successfully");
+
+        } catch (error) {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            alert(errorCode, errorMessage);
         }
 
+    };
 
-    }
 
-    onSubmit = async () => {
-        database()
-            .ref(`/questions`)
-            .set({
-                title: this.state.title,
-                category: this.state.category,
-                description: this.state.description,
-                createdBy: UserID
-            })
-            .then(() => {
-                this.setState({
-                    showModel: true
-                })
-                setTimeout(() => {
-                    this.setState({
-                        showModel: false
-                    }, () => {
-                        this.props.navigation.goBack()
-                    })
-                }, 1000)
-            })
-            .catch(err => console.log(err))
-    }
 
-    rnderModal = () => {
+    // onSubmit = async () => {
+    //     database()
+    //         .ref(`/questions`)
+    //         .set({
+    //             title: this.state.title,
+    //             category: this.state.category,
+    //             description: this.state.description,
+    //             createdBy: "10"
+    //         })
+    //         .then(() => {
+    //             this.setState({
+    //                 showModel: true
+    //             })
+    //             setTimeout(() => {
+    //                 this.setState({
+    //                     showModel: false
+    //                 }, () => {
+    //                     this.props.navigation.goBack()
+    //                 })
+    //             }, 1000)
+    //         })
+    //         .catch(err => console.log(err))
+    // }
+
+    const rnderModal = () => {
         return (
             <Modal
                 style={{
@@ -58,11 +79,9 @@ export default class AddQuestionScreen extends Component {
                 }}
                 animationType="fade"
                 transparent={true}
-                visible={this.state.showModel}
+                visible={showModel}
                 onRequestClose={() => {
-                    this.setState({
-                        showModel: false
-                    })
+                    setShowModel(false)
                 }}
             >
                 <View style={styles.centeredView}>
@@ -74,7 +93,7 @@ export default class AddQuestionScreen extends Component {
                             </TouchableOpacity>
                         </View> */}
                         <View style={{ alignItems: 'center', paddingTop: 30 }}>
-                            <Image source={require('../../assests/Images/succuess.png')} style={{ height: 60, width: 60 }} />
+                            {/* <Image source={require('../../assests/Images/succuess.png')} style={{ height: 60, width: 60 }} /> */}
                         </View>
                         <View>
                             <List.Item
@@ -94,65 +113,65 @@ export default class AddQuestionScreen extends Component {
     }
 
 
-
-    render() {
-        return (
-            <View style={{ flex: 1, width: "100%", height: "100%", backgroundColor: "#FBFCFC", paddingTop: StatusBar.currentHeight }}>
-                {this.rnderModal()}
-                <View style={{ width: '100%', flexDirection: 'row' }}>
-                    <TouchableOpacity style={{ width: '13%', height: 70, alignItems: 'flex-end', justifyContent: 'center' }} onPress={() => this.props.navigation.goBack()}>
-                        <Ionicons color={"rgba(114, 120, 245, 1)"} size={26} name={"chevron-back"} />
-                    </TouchableOpacity>
-                    <View style={{ width: '60%', height: 70, justifyContent: 'center', alignItems: 'flex-start' }}>
-                        <Text style={{ color: 'rgba(114, 120, 245, 1)', fontWeight: 'bold', fontSize: 20, paddingStart: 5 }}>{'Submit Answer'}</Text>
-                    </View>
+    return (
+        <View style={{ flex: 1, width: "100%", height: "100%", backgroundColor: "#FBFCFC", paddingTop: StatusBar.currentHeight }}>
+            {rnderModal()}
+            <View style={{ width: '100%', flexDirection: 'row' }}>
+                <TouchableOpacity style={{ width: '13%', height: 70, alignItems: 'flex-end', justifyContent: 'center' }} onPress={() => navigation.goBack()}>
+                    <Ionicons color={"rgba(114, 120, 245, 1)"} size={26} name={"chevron-back"} />
+                </TouchableOpacity>
+                <View style={{ width: '60%', height: 70, justifyContent: 'center', alignItems: 'flex-start' }}>
+                    <Text style={{ color: 'rgba(114, 120, 245, 1)', fontWeight: 'bold', fontSize: 20, paddingStart: 5 }}>{'Ask New Question'}</Text>
                 </View>
-                <ImageBackground style={{ width: '100%', height: '90%' }} source={require('../../assests/Images/background.png')}>
-                    <KeyboardAwareScrollView style={{ paddingStart: 14, paddingEnd: 14, marginBottom: 20 }}>
-
-                        <View style={{ padding: 13, }}>
-                            <TextInput
-                                label="Title"
-                                value={this.state.title}
-                                onChangeText={text => this.setState({ title: text })}
-                                multiline={true}
-                                numberOfLines={10}
-                            />
-                        </View>
-                        <View style={{ padding: 13, }}>
-                            <TextInput
-                                label="Title"
-                                value={this.state.title}
-                                onChangeText={text => this.setState({ title: text })}
-                                multiline={true}
-                                numberOfLines={10}
-                            />
-                        </View>
-                        <View style={{ padding: 13, }}>
-                            <TextInput
-                                label="Description"
-                                value={this.state.description}
-                                onChangeText={text => this.setState({ title: text })}
-                                multiline={true}
-                                numberOfLines={10}
-                            />
-                        </View>
-                        <View style={{ width: '100%', alignItems: 'flex-end', paddingTop: 20, marginBottom: 10, padding: 13 }}>
-                            <Button
-                                mode="contained"
-                                dark={false}
-                                color={'rgba(114, 120, 245, 0.64)'}
-                                disabled={this.state.answer === null || this.state.answer.length === 0 ? true : false}
-                                onPress={() => this.onSubmit()} >
-                                Submit
-                            </Button>
-                        </View>
-                    </KeyboardAwareScrollView>
-                </ImageBackground>
             </View>
-        )
-    }
+            <ImageBackground style={{ width: '100%', height: '90%' }}
+            // source={require('../../assests/Images/background.png')}
+            >
+                <KeyboardAwareScrollView style={{ paddingStart: 14, paddingEnd: 14, marginBottom: 20 }}>
+
+                    <View style={{ padding: 13, }}>
+                        <TextInput
+                            label="Title"
+                            value={title}
+                            onChangeText={text => setTitle(text)}
+                        />
+                    </View>
+                    <View style={{ padding: 13, }}>
+                        <Picker
+                            selectedValue={category}
+                            style={{ height: 50, width: 300, padding: 5 }}
+                            onValueChange={(itemValue, itemIndex) => setCategory(itemValue)}
+                        >
+                            <Picker.Item label="-- Select Category" value="-- Select Category" />
+                            <Picker.Item label="Tax" value="Tax" />
+                            <Picker.Item label="Intellectual Property" value="Intellectual Property" />
+                        </Picker>
+                    </View>
+                    <View style={{ padding: 13, }}>
+                        <TextInput
+                            label="Description"
+                            value={description}
+                            onChangeText={text => setDescription(text)}
+                            multiline={true}
+                            numberOfLines={10}
+                        />
+                    </View>
+                    <View style={{ width: '100%', alignItems: "center", paddingTop: 20, top: 40, padding: 13 }}>
+                        <Button
+                            mode="contained"
+                            dark={false}
+                            color={'rgba(114, 120, 245, 0.64)'}
+                            // disabled={this.state.answer === null || this.state.answer.length === 0 ? true : false}
+                            onPress={handleSubmit} >
+                            Submit
+                        </Button>
+                    </View>
+                </KeyboardAwareScrollView>
+            </ImageBackground>
+        </View>
+    )
 }
+
 
 const styles = StyleSheet.create({
     centeredView: {
--- src/components/Anuka/AnukaRootNavigator.js
+++ src/components/Anuka/AnukaRootNavigator.js
@@ -1,11 +1,6 @@
 import { Text, View } from 'react-native'
 import React, { Component } from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import QuestionListScreen from './MyQuestionListScreen';
-import ViewQuestionScreen from './ViewQuestionScreen';
-import AddAnswerScreen from './AddAnswerScreen';
-import UpdateAnswerScreen from './UpdateAnswerScreen';
-
 
 
 import AddQuestionScreen from './AddQuestionScreen';
@@ -17,17 +12,8 @@ export default class AnukaRootNavigator extends Component {
   render() {
     return (
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="QuestionListScreen" component={QuestionListScreen} />
-        <Stack.Screen name="ViewQuestionScreen" component={ViewQuestionScreen} />
-        <Stack.Screen name="AddAnswerScreen" component={AddAnswerScreen} />
-        <Stack.Screen name="UpdateAnswerScreen" component={UpdateAnswerScreen} />
-
         <Stack.Screen name="AddQuestionScreen" component={AddQuestionScreen} />
         <Stack.Screen name="MyQuestionListScreen" component={MyQuestionListScreen} />
-
-
-
-
       </Stack.Navigator>
     )
   }
